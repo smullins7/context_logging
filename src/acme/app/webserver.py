6e645e4577e67022c0e669ca978972dacf75b26b
Stephen Mullins <smullins7@gmail.com>
2012-05-04 21:28:57 -0700

    Using context filter to inject client IP

--- src/acme/app/webserver.py
+++ src/acme/app/webserver.py
@@ -2,13 +2,22 @@
 import logging
 import web
 
-logging.basicConfig(level=logging.INFO)
+class ContextFilter(logging.Filter):
+    def filter(self, record):
+        record.client_ip = web.ctx.env['REMOTE_ADDR']
+        return True
+
+context_filter = ContextFilter()
+
+FORMAT = "%(asctime)s,%(msecs)d|%(levelname)s|%(client_ip)s|%(name)s|%(funcName)s|%(message)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO)
 logger = logging.getLogger(__name__)
+logger.addFilter(context_filter)
 
 class IndexController(object):
 
     def GET(self):
-        logger.info("Received request from %s" % web.ctx.env['REMOTE_ADDR'])
+        logger.info("Received request")
         return "Hello, world!"
 
 urls = (
