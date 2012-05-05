f8d11983ab1c6d47fc8f69e6fe9391ee2a77a1c5
Stephen Mullins <smullins7@gmail.com>
2012-05-04 21:22:57 -0700

    Adding log message with client ip address

--- src/acme/app/webserver.py
+++ src/acme/app/webserver.py
@@ -1,9 +1,14 @@
 
+import logging
 import web
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class IndexController(object):
 
     def GET(self):
+        logger.info("Received request from %s" % web.ctx.env['REMOTE_ADDR'])
         return "Hello, world!"
 
 urls = (
